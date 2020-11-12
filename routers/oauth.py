79d26fdb18aaf99654e1aa88687899b5d3f4d367
LcferShell <71859305+LcfherShell@users.noreply.github.com>
2020-11-12 21:08:41 +0700

    Update oauth.py

--- routers/oauth.py
+++ routers/oauth.py
@@ -5,8 +5,8 @@ import json
 
 GOOGLE   = {
             'name'  : str('google'),
-            'key'   : str('48161273198-k268u9r7n5mlhrbdrpj8qss56vqht9qp.apps.googleusercontent.com'),
-            'secret': str('65vVDEjHu4si0BMoEKiWIRKs')
+            'key'   : str(''),
+            'secret': str('')
            }
 
 FACEBOOK = {
@@ -17,8 +17,8 @@ FACEBOOK = {
 
 GITHUB   = {
             'name'  : str('github'),
-            'key'   : str('b4441aac9278439c72ed'),
-            'secret': str('efee60300dd6c1810f37f3f970c069e7e1f6ea62')
+            'key'   : str(''),
+            'secret': str('')
            }
 
 
@@ -48,23 +48,7 @@ PAYPAL = {
            }
 
 RECAPTCHA = {
-            'key'   : str('6Lfci9QZAAAAAJdzaCsQjOZ6od3Jhr4cNjPxDMUE'), 
-            'secret': str('6Lfci9QZAAAAAC8SHmti-WgDfqdvr4LbSl2xYrwb'), 
+            'key'   : str(''), 
+            'secret': str(''), 
             'enabled': True
             }
-
-
-#RECAPTCHA_ENABLED
-#from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
-#from flask_dance.contrib.github import make_github_blueprint, github
-
-#githubs = make_github_blueprint(client_id='b4441aac9278439c72ed', client_secret='c06f0615464423ce78bd54de9717923ccf5be737')
-#app.register_blueprint(githubs, url_prefix='/access')
-#@app.route('/login/github')
-#def github():
-#    if not github.authorized:
-#        return redirect(url_for('github.login'))
-#    userinfo = github.get('/user')
-#    if userinfo.ok:
-#        parsejson =  userinfo.json()
-#        return 'your name {}'.format(parsejson['login'])
